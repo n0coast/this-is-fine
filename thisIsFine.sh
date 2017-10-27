d4cd3e4540c1e866a6b14da1c89333307c654d40
Nate Tade <ntade@rei.com>
2017-10-27 10:31:01 -0700

    refinements

--- thisIsFine.sh
+++ thisIsFine.sh
@@ -1,4 +1,7 @@
-for ((i=1 ; i<=$(wc -l thisIsFine.txt|awk '{print $1}'); i++)); do
-  sed -n ${i}p thisIsFine.txt
+#!/bin/bash
+baseDir="/opt/this-is-fine"
+asciiFile="${baseDir}/thisIsFine.txt"
+for ((i=1 ; i<=$(wc -l ${asciiFile}|awk '{print $1}'); i++)); do
+  >&2 sed -n ${i}p ${asciiFile}
   sleep .1
 done
